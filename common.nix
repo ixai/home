14cd86201dbd44d0a8bd22daa0e76eb60421c2c9
Ixai Lanzagorta <ixai.lanzagorta@gmail.com>
2026-05-01 09:25:18 +0900

    Install tmux

--- common.nix
+++ common.nix
@@ -107,4 +107,55 @@ in
       pager.branch = true;
     };
   };
+
+  programs.tmux = {
+    enable = true;
+
+    baseIndex = 1;
+    clock24 = true;
+    historyLimit = 10000;
+    keyMode = "vi";
+    mouse = true;
+    # newSession = true;
+    prefix = "C-a";
+    shell = "${pkgs.zsh}/bin/zsh";
+
+    plugins = with pkgs.tmuxPlugins; [
+      {
+        plugin = catppuccin;
+        extraConfig = "set -g @catppuccin_flavor 'frappe'";
+      }
+      sensible
+      yank
+    ];
+
+    extraConfig = ''
+      # set-option -ga terminal-overrides ",*:Tc"   # enable terminfo true colors
+      set-option -g default-command "''${SHELL}"  # start a non-login shell
+
+      # Extended keys
+      set-option -g extended-keys on                          # extended keys (ctrl+enter, shift+enter, ...)
+      set-option -g extended-keys-format csi-u                # kitty keyboard protocol
+      set-option -as terminal-features 'xterm-ghostty:csi-u'  # Ghostty speaks Kitty natively
+
+      # Windows
+      bind-key c new-window -c "#{pane_current_path}" # retain cwd on new window
+
+      # Panes
+      bind-key C-l split-window -h -c "#{pane_current_path}"  # split pane right
+      bind-key C-j split-window -v -c "#{pane_current_path}"  # split pane down
+
+      bind-key -n C-S-h select-pane -L -Z
+      bind-key -n C-S-j select-pane -D -Z
+      bind-key -n C-S-k select-pane -U -Z
+      bind-key -n C-S-l select-pane -R -Z
+
+      bind-key -n C-S-Left resize-pane -L
+      bind-key -n C-S-Down resize-pane -D
+      bind-key -n C-S-Up resize-pane -U
+      bind-key -n C-S-Right resize-pane -R
+
+      bind-key -n C-S-\; resize-pane -Z
+    '';
+  };
 }
