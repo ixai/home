7c17a5d63af8b65c1f3e8cf0e17b55a8003fb4f2
Ixai Lanzagorta <ixai@mercari.com>
2026-07-30 15:33:42 +0900

    Configure topgrade (assume-yes, ask-retry)

--- topgrade.nix
+++ topgrade.nix
@@ -6,12 +6,15 @@
       misc = {
         # Disable updates for nix-managed binaries
         disable = [
+          "claude_code"
+          "gcloud"
           "tmux"
           "uv"
-          "claude_code"
         ];
 
         cleanup = true;
+        assume_yes = true;
+        ask_retry = false;
       };
     };
   };
