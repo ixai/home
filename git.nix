bb4421801e6df949a37ddc405865bb1f5aa1e252
Ixai Lanzagorta <ixai@mercari.com>
2026-09-18 22:10:03 +0900

    Use hunk as git difftool

--- git.nix
+++ git.nix
@@ -8,6 +8,8 @@
       init.defaultBranch = "main";
       pager.branch = true;
       core.pager = "hunk pager";
+      diff.tool = "hunk";
+      difftool.hunk.cmd = ''hunk difftool "$LOCAL" "$REMOTE" "$MERGED"'';
 
       # Launch the difftool without prompting before each file.
       difftool.prompt = false;
